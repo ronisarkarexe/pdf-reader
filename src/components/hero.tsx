5909fca277efc1217b8d6d81f5b3a090ea52b143
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-06-21 15:27:48 +0530

    change design

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import FileUploader from "./file_uploader";
+import { ShieldCheck, TriangleAlert } from "lucide-react";
 
 export default function HeroSection(props: {
   setIsUpdate: (value: boolean) => void;
@@ -22,16 +23,44 @@ export default function HeroSection(props: {
             </strong>
           </motion.h1>
 
-          <motion.p
-            className="mt-4 sm:text-xl/relaxed text-sm text-gray-400"
+          <motion.span
+            className="mt-4 sm:text-xl/relaxed text-gray-400 text-base"
             initial={{ opacity: 0, x: -100 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ type: "spring", damping: 18, mass: 0.75, delay: 0.2 }}
           >
-            PDF Reader is free open-source application, that help you read pdf
-            like book!
-          </motion.p>
+            PDF Reader is a free, open-source application designed to enhance
+            your reading experience with PDF documents!
+          </motion.span>
           <FileUploader setIsUpdate={props.setIsUpdate} />
+
+          <div className="flex items-center justify-between">
+            <motion.div
+              className="flex items-center gap-2 mt-2"
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ type: "spring", damping: 18, mass: 0.75 }}
+            >
+              <TriangleAlert className="h-4 w-4 text-red-600" />{" "}
+              <span className="text-xs text-gray-400">
+                {" "}
+                PDF name should not have space: Test file.pdf
+              </span>
+            </motion.div>
+
+            <motion.div
+              className="flex items-center gap-2 mt-2"
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ type: "spring", damping: 18, mass: 0.75 }}
+            >
+              <ShieldCheck className="h-4 w-4 text-blue-600" />{" "}
+              <span className="text-xs text-gray-400">
+                {" "}
+                PDF name: Test_file.pdf
+              </span>
+            </motion.div>
+          </div>
         </div>
       </div>
     </section>
